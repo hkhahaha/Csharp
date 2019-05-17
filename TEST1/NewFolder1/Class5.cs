e04bb212d61f4cf5b2f2d0239c18d1f085e37034
hkhahaha <931000971@qq.com>
2019-05-17 16:52:51 +0800

    第二次提交啦

---
+++ TEST1/NewFolder1/Class5.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TEST1.NewFolder1
+{
+    class Class5
+    {
+        static void Main()
+        {
+            Random a = new Random();
+            int[] mark = new int[100];
+            int avg = 0;
+            int sum=0;
+            for(int i = 0; i < 100; i++)
+            {
+                mark[i] = a.Next(101);
+                Console.WriteLine(mark[i]);
+                sum = sum + mark[i];
+            }
+            avg = sum / 100;
+            Console.WriteLine(avg+"123");
+            Console.WriteLine(sum+"123");
+
+        }
+    }
+}
---
+++ TEST1/NewFolder1/Class6.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TEST1.NewFolder1
+{
+    class Class6
+    {
+        static void Main()
+        {
+            double d = 123.45;
+            Console.WriteLine(Convert.ToBoolean(d));
+        }
+    }
+}
